2a4d0c3a74537fd52ac5607931995a18181d93e8
Cosingf <1109400576@qq.com>
2019-12-23 14:32:37 +0800

    更新高亮搜索算法

--- legaldocument/src/main/java/cn/xmu/edu/legaldocument/service/ReadService.java
+++ legaldocument/src/main/java/cn/xmu/edu/legaldocument/service/ReadService.java
@@ -8,11 +8,14 @@ import cn.xmu.edu.legaldocument.mapper.PageMapper;
 import cn.xmu.edu.legaldocument.mapper.QAMapper;
 import cn.xmu.edu.legaldocument.mapper.SectionMapper;
 import cn.xmu.edu.legaldocument.mapper.SelectionMapper;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
+
+
 
 @Service
 public class ReadService {
@@ -34,47 +37,30 @@ public class ReadService {
         Page page =pageMapper.selectByBookIdAndPageNum(bookid,pagenum);
         List<QA> qas = new ArrayList<>();
         if (page!=null) {
-            Selection selection = new Selection();
-            selection.setSelection(highLight);
-            selection.setPageId(page.getId());
-            selection.setBookId(bookid);
-            selectionMapper.insert(selection);
-            List<QASection> results = new ArrayList<>();
-            String[] words = highLight.split(" ");
-            List<String> inputStr = new ArrayList<>();
-            String strNum = "";
-            String str1="";String str2="";
-            if (words.length==1) {
-                str1=words[0];
-                strNum ="1";
-            }//当高亮词小于十个，就把其划分成两个str，模糊查询
-            else if (words.length<=10){
-                int n = words.length/2;
-                String str=words[0];
-                for (int i=1;i<n;i++)
-                     str=str+" "+words[i];
-                str1=str;
-                str=words[n];
-                for (int i=n+1;i<words.length;i++)
-                    str=str+" "+words[i];
-                str2=str;
-                strNum ="2";
+            List<Section> sections;
+            sections = sectionMapper.selectByPageId(page.getId());
+            Map<Long,Float> sectionSim = new HashMap<>();
+            for (Section section :sections)
+            {
+                float a=(float) calculateStringDistance(highLight,section.getSectionContent())/Math.max(highLight.length(),section.getSectionContent().length());
+                sectionSim.put(section.getId(),1-a);
             }
-            //若高亮词大于十，取前是个划分成两个str，模糊查询，先定位出具体位置
-            else {
-                String str=words[0];
-                for (int i=1;i<5;i++)
-                    str=str+" "+words[i];
-               str1 =str;
-                str=words[5];
-                for (int i=6;i<10;i++)
-                    str=str+" "+words[i];
-                 str2=str;
-                strNum ="2";
-            }
-            List<Section> sections = sectionMapper.selectByPageIdAndHighLightAndNum(page.getId(),str1,str2,strNum);
-            Section section = sections.get(1);
-             qas = qaMapper.selectQASectionBySectionId(section.getId());
+            List<Map.Entry<Long ,Float>> sectionSimList = new ArrayList<Map.Entry<Long ,Float>>(sectionSim.entrySet());
+            Collections.sort(sectionSimList,new Comparator<Map.Entry<Long ,Float>>(){
+                @Override
+                public int compare(Map.Entry<Long, Float> o1,Map.Entry<Long, Float> o2) {
+                    Float p =(o2.getValue()-o1.getValue());
+                    if(p>0){
+                        return 1;
+                    }
+                    else if(p==0){
+                        return 0;
+                    }
+                    else
+                        return -1;
+                }
+            });
+            qas = qaMapper.selectQASectionBySectionId(sectionSimList.get(0).getKey());
         }
         return qas;
     }
@@ -103,6 +89,58 @@ public class ReadService {
             }
         }
         return  pageSectionVOs;
+    }
+
+
+    private static int calculateStringDistance(@NotNull String s1, String s2) {
+        int Distance=0;
+        int Length1 =s1.length();
+        int Length2 =s2.length();
+        if (Length1==0) {
+            Distance=Length2;
+        }
+        if(Length2==0)
+        {
+            Distance=Length1;
+        }
+        if(Length1!=0&&Length2!=0) {
+            int[][] Distance_Matrix = new int[Length1 + 1][Length2 + 1];
+            //编号
+            int Bianhao = 0;
+            for (int i = 0; i <= Length1; i++) {
+                Distance_Matrix[i][0] = Bianhao;
+                Bianhao++;
+            }
+            Bianhao = 0;
+            for (int i = 0; i <= Length2; i++) {
+                Distance_Matrix[0][i] = Bianhao;
+                Bianhao++;
+            }
+
+            char[] Str_1_CharArray = s1.toCharArray();
+            char[] Str_2_CharArray = s2.toCharArray();
+
+
+            for (int i = 1; i <= Length1; i++) {
+                for (int j = 1; j <= Length2; j++) {
+                    if (Str_1_CharArray[i - 1] == Str_2_CharArray[j - 1]) {
+                        Distance = 0;
+                    } else {
+                        Distance = 1;
+                    }
+
+                    int Temp1 = Distance_Matrix[i - 1][j] + 1;
+                    int Temp2 = Distance_Matrix[i][j - 1] + 1;
+                    int Temp3 = Distance_Matrix[i - 1][j - 1] + Distance;
+
+                    Distance_Matrix[i][j] = Temp1 > Temp2 ? Temp2 : Temp1;
+                    Distance_Matrix[i][j] = Distance_Matrix[i][j] > Temp3 ? Temp3 : Distance_Matrix[i][j];
+
+                }
+            }
+            Distance= Distance_Matrix[Length1][Length2];
+        }
+        return Distance;
     }
 
 }
